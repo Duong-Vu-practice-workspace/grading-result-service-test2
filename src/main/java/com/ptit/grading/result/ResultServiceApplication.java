996fe8fde9d5b1725361ffa018ddfe64b7176370
Duong Vu Cong Tuan <vucongtuanduong2409@gmail.com>
2026-06-09 10:03:40 +0700

    Fix @EntityScan to include service model package

--- src/main/java/com/ptit/grading/result/ResultServiceApplication.java
+++ src/main/java/com/ptit/grading/result/ResultServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("com.ptit.grading.common.model")
+@EntityScan({"com.ptit.grading.common.model", "com.ptit.grading.result.model"})
 public class ResultServiceApplication {
 
     public static void main(String[] args) {
